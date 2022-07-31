7d5578f5ee93c705da72affab3f642f2b7a97153
Derrick Hammer <derrick@derrickhammer.com>
2022-07-31 00:55:16 -0400

    *Add more types from webextension

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,20 @@
 import { WebRequest, Proxy } from "webextension-polyfill";
 import OnHeadersReceivedDetailsType = WebRequest.OnHeadersReceivedDetailsType;
 import OnBeforeRequestDetailsType = WebRequest.OnBeforeRequestDetailsType;
+import OnBeforeSendHeadersDetailsType = WebRequest.OnBeforeSendHeadersDetailsType;
 import OnCompletedDetailsType = WebRequest.OnCompletedDetailsType;
 import OnErrorOccurredDetailsType = WebRequest.OnErrorOccurredDetailsType;
 import BlockingResponse = WebRequest.BlockingResponse;
 import OnRequestDetailsType = Proxy.OnRequestDetailsType;
+import HttpHeaders = WebRequest.HttpHeaders;
 
 export {
   OnHeadersReceivedDetailsType,
   OnBeforeRequestDetailsType,
+  OnBeforeSendHeadersDetailsType,
   OnCompletedDetailsType,
   OnErrorOccurredDetailsType,
   BlockingResponse,
   OnRequestDetailsType,
+  HttpHeaders,
 };
