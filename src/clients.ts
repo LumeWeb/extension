3b559efabc6db39bf5511c77362fee5faeebbf08
Derrick Hammer <derrick@derrickhammer.com>
2023-04-09 19:43:22 -0400

    * Add two new imports and export two new clients: swarmClient and peerDiscoveryClient.

--- src/clients.ts
+++ src/clients.ts
@@ -1,7 +1,11 @@
 import { createClient as createDnsClient } from "@lumeweb/kernel-dns-client";
 import { createClient as createIpfsClient } from "@lumeweb/kernel-ipfs-client";
+import { createClient as createSwarmClient } from "@lumeweb/kernel-swarm-client";
+import { createClient as createPeerDiscoveryClient } from "@lumeweb/kernel-peer-discovery-client";
 
 const dnsClient = createDnsClient();
 const ipfsClient = createIpfsClient();
+const swarmClient = createSwarmClient();
+const peerDiscoveryClient = createPeerDiscoveryClient();
 
-export { dnsClient, ipfsClient };
+export { dnsClient, ipfsClient, swarmClient, peerDiscoveryClient };
