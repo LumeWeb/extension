9de130107aa6ed631dd2246a5aa7322e27e2eedc
Derrick Hammer <derrick@derrickhammer.com>
2023-08-13 14:58:26 -0400

    fix: update social links

--- ui/apps/dashboard/components/Footer.jsx
+++ ui/apps/dashboard/components/Footer.jsx
@@ -3,23 +3,28 @@ import classNames from "classnames";
 import svgGithub from "../../../assets/icon/github.svg?raw";
 import svgDiscord from "../../../assets/icon/discord.svg?raw";
 import svgTwitter from "../../../assets/icon/twitter.svg?raw";
-import svgFacebook from "../../../assets/icon/facebook.svg?raw";
 import parse from "html-react-parser";
 
 export default function Footer({ connected }) {
   return (
     <div className={classNames("socials", { connected })}>
-      <a href="#" title="GitHub" className="github-logo">
+      <a
+        href="https://github.com/LumeWeb"
+        title="GitHub"
+        className="github-logo">
         {parse(svgGithub)}
       </a>
-      <a href="#" title="Discord" className="discord-logo">
+      <a
+        href="https://discord.gg/qpC8ADp3rS"
+        title="Discord"
+        className="discord-logo">
         {parse(svgDiscord)}
       </a>
-      <a href="#" title="Twitter" className="twitter-logo">
+      <a
+        href="https://twitter.com/lumeweb3"
+        title="Twitter"
+        className="twitter-logo">
         {parse(svgTwitter)}
-      </a>
-      <a href="#" title="Facebook" className="facebook-logo">
-        {parse(svgFacebook)}
       </a>
     </div>
   );
